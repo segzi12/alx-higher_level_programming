bcab448435335586a0711fd7c56f6963e8f48760
Idowu Segun <segunidowu682@gmail.com>
2024-04-24 00:00:54 -0700

    uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for letter in c:
+        ascii_value = ord(letter)
+        if 97 <= ascii_value <= 122:
+            ascii_value -= 32
+        print("{}".format(chr(ascii_value)), end="")
+    print()
