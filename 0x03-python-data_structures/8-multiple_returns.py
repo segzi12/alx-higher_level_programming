112f3da32f3919237a1faf7fff492d53b36fbd7f
Idowu Segun <segunidowu682@gmail.com>
2024-04-28 13:42:10 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        return (None, None)
+    else:
+        length = len(sentence)
+        first = sentence[0]
+        return (length, first)
